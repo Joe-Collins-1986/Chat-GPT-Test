6d976aa2c8d5165dd7506292d3d57b7513703007
Joesph Collins <collins.jw@btinternet.com>
2024-02-22 13:09:25 +0000

    build proxy server to api calls

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://valantines-drf-8626efa242cc.herokuapp.com/";
+axios.defaults.baseURL = "/api/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
