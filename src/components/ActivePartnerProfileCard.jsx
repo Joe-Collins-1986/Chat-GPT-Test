d09f7843eaf11e214ba53870024c3d0f8dead29a
Joesph Collins <collins.jw@btinternet.com>
2024-03-05 17:01:17 +0000

    add dividers to partner profile sections

--- src/components/ActivePartnerProfileCard.jsx
+++ src/components/ActivePartnerProfileCard.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { usePartnerProfile } from "../contexts/PartnerProfileContext";
-import { Card, CardBody, CardHeader, Heading, Box } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Box,
+  HStack,
+  Avatar,
+  Button,
+  Divider,
+} from "@chakra-ui/react";
 
 import PartnerVariableSelection from "./PartnerVariableSelection";
 
@@ -24,26 +34,45 @@ const ActivePartnerProfileCard = () => {
     );
   }
 
+  console.log(partnerProfile.activeProfile);
+
+  const { image, name, about } = partnerProfile.activeProfile;
+
   return (
     <Card borderRadius="0 25px 0 25px" color={"white"}>
       <CardHeader
         borderRadius="0 25px 0 0px"
-        bgGradient={"linear(to-l, themeCustom.400, themeCustom.900)"}
+        bgGradient={"linear(to-l, themeCustom.200, themeCustom.900)"}
       >
-        <Heading mb={5} size="lg">
-          Active Partner Profile
-        </Heading>
-        <Heading size="sm">{partnerProfile.activeProfile.name}</Heading>
+        <HStack justifyContent="space-between">
+          <Heading mb={5} size="lg">
+            Active Partner Profile
+          </Heading>
+          <Button>Delete</Button>
+        </HStack>
+
+        <HStack>
+          <Avatar name={name} src={image} />
+          {name?.length > 7 ? (
+            <Heading size="sm">{name.slice(0, 7)}...</Heading>
+          ) : (
+            <Heading size="sm">{name}</Heading>
+          )}
+        </HStack>
       </CardHeader>
-      <CardBody>
+
+      <CardBody color="black">
         <Box>
           <Heading size="sm">Partner Description</Heading>
-          <p>
-            {partnerProfile.activeProfile.description
-              ? partnerProfile.activeProfile.description
-              : "No description added"}
-          </p>
+          <p>{about ? about : "No description added"}</p>
         </Box>
+
+        <Divider
+          mt={5}
+          borderWidth="5px"
+          borderRadius="10"
+          borderColor="themeCustom.900"
+        />
 
         <PartnerVariableSelection
           name="likes"
@@ -51,10 +80,24 @@ const ActivePartnerProfileCard = () => {
           endPoint="/partner-likes/"
         />
 
+        <Divider
+          mt={5}
+          borderWidth="5px"
+          borderRadius="10"
+          borderColor="themeCustom.900"
+        />
+
         <PartnerVariableSelection
           name="characteristics"
           mapLocation="characteristics_display"
           endPoint="/partner-characteristics/"
+        />
+
+        <Divider
+          mt={5}
+          borderWidth="5px"
+          borderRadius="10"
+          borderColor="themeCustom.900"
         />
 
         {partnerProfile.activeProfile.passions.length > 0 ? (
