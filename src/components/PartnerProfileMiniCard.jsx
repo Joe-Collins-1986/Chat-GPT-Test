7bc4c3a1856fbac748872e89233d28f865806f2d
Joesph Collins <collins.jw@btinternet.com>
2024-03-08 10:04:23 +0000

    grey active button and diable click functionality

--- src/components/PartnerProfileMiniCard.jsx
+++ src/components/PartnerProfileMiniCard.jsx
@@ -9,10 +9,14 @@ import {
 } from "@chakra-ui/react";
 import useDeletePartnerProfile from "../hooks/usePartnerProfileDelete.Hk";
 import useActivateHk from "../hooks/useActivateHk";
+import { useUserProfile } from "../contexts/UserProfileContext";
 
 const PartnerProfileMiniCard = ({ name, id, image }) => {
   const handleDelete = useDeletePartnerProfile();
   const handleActivate = useActivateHk();
+
+  const { userProfile } = useUserProfile();
+  console.log("print", userProfile.active_partner_profile_id);
 
   return (
     <Card
@@ -33,7 +37,14 @@ const PartnerProfileMiniCard = ({ name, id, image }) => {
             )}
           </HStack>
           <HStack>
-            <Button onClick={() => handleActivate(id)}>Activate</Button>
+            {userProfile.active_partner_profile_id === id ? (
+              <Button bgGradient="linear(to-l, gray.300, gray.900)">
+                Active
+              </Button>
+            ) : (
+              <Button onClick={() => handleActivate(id)}>Activate</Button>
+            )}
+
             <Button
               bgGradient="linear(to-l, red.300, themeCustom.900)"
               onClick={() => handleDelete(id)}
