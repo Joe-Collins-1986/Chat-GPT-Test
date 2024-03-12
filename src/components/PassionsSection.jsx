0a4b1d78af4fa0513e2de59be3aace1b1cc0f7b4
Joesph Collins <collins.jw@btinternet.com>
2024-03-12 16:08:20 +0000

    delete passions

--- src/components/PassionsSection.jsx
+++ src/components/PassionsSection.jsx
@@ -7,17 +7,44 @@ import {
   HStack,
   Alert,
   AlertIcon,
+  Card,
+  Text,
 } from "@chakra-ui/react";
 
 import { usePartnerProfile } from "../contexts/PartnerProfileContext";
 
 import usePassionsCreateHook from "../hooks/usePassionsCreateHk";
 
+import { axiosReq } from "../api/axiosDefault";
+
+import { useSetPartnerProfile } from "../contexts/PartnerProfileContext";
+
 const PassionsSection = () => {
+  const setPartnerProfile = useSetPartnerProfile();
+
   const { partnerProfile, partnerProfileLoading } = usePartnerProfile();
 
   const { passion, handleChange, handleSubmit, error } =
     usePassionsCreateHook();
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await axiosReq.delete(`/partner-passions/${id}/`);
+      console.log(response);
+
+      setPartnerProfile((prev) => ({
+        ...prev,
+        activeProfile: {
+          ...prev.activeProfile,
+          passions: prev.activeProfile.passions.filter(
+            (passion) => passion.id !== id
+          ),
+        },
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   if (partnerProfileLoading) {
     return <div>Loading...</div>;
@@ -55,16 +82,16 @@ const PassionsSection = () => {
         ) : null}
       </form>
 
-      {/* <input type="text" placeholder="Add passion" />
-      <Button>Add</Button> */}
-
       {partnerProfile.activeProfile.passions?.length > 0 ? (
         <Box mt={5}>
-          <ul>
-            {partnerProfile.activeProfile.passions.map((passion) => (
-              <li key={passion.id}>{passion.passion_text}</li>
-            ))}
-          </ul>
+          {partnerProfile.activeProfile.passions.map((passion) => (
+            <Card key={passion.id} p={3} mb={3}>
+              <HStack justifyContent={"space-between"}>
+                <Text>{passion.passion_text}</Text>
+                <Button onClick={() => handleDelete(passion.id)}>Del</Button>
+              </HStack>
+            </Card>
+          ))}
         </Box>
       ) : (
         <Box mt={5}>
